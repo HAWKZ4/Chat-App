b3ed404dc27015f25eab3f6236fdff82f61f386c
Web Ghost <108879264+HAWKZ4@users.noreply.github.com>
2024-06-17 13:11:38 +0300

    feat: Add some messages and activity detection feature
    - Add a welcome message for each user upon connection to the server
    
    - Add a message including the name of the user who connected to the server
    
    - Add a message including the name of the user who disconnected from the server
    
    - Add typing activity notification to inform users when someone is typing

--- server/public/app.js
+++ server/public/app.js
@@ -1,20 +1,44 @@
 const socket = io("ws://localhost:3500");
 
+const activity = document.querySelector(".activity");
+const msgInput = document.querySelector("input");
+
 function sendMessage(e) {
   e.preventDefault();
-  const input = document.querySelector("input");
-  if (input.value) {
-    socket.emit("message", input.value);
-    input.value = "";
+  if (msgInput.value) {
+    socket.emit("message", msgInput.value);
+    msgInput.value = "";
   }
-  input.focus();
+  msgInput.focus();
 }
 
 document.querySelector("form").addEventListener("submit", sendMessage);
 
 // Listen for messages
 socket.on("message", (data) => {
+  activity.textContent = "";
   const li = document.createElement("li");
   li.textContent = data;
   document.querySelector("ul").appendChild(li);
+});
+
+// 1
+msgInput.addEventListener("keypress", () => {
+  // Send activity to the server on keypress event
+  socket.emit("activity", socket.id.substring(0, 5));
+});
+
+let activityTimer;
+
+// 3
+// Listen for activity
+socket.on("activity", (name) => {
+  activity.textContent = `${name} is typing...`;
+
+  clearTimeout(activityTimer);
+
+  // Clear after 3 seconds
+  activityTimer = setTimeout(() => {
+    activity.textContent = "";
+  }, 3000);
 });
